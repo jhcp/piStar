791b79b534de693e4757ee0ecca0e97d060c90b1
João Pimentel <joaozinho@gmail.com>
2019-05-21 21:28:30 -0300

    hide empty buttons, allow hidden links
    - if a there is no container, no dependency type, or no actor link, hide the respective 'add' button
    - allow the creation of links that cannot be added through the regular 'add link' UI

--- tool/app/ui/controllers/addButton.js
+++ tool/app/ui/controllers/addButton.js
@@ -14,52 +14,64 @@ ui.components.createAddButtons = function() {
     //create the ADD buttons
 
     //create Add <<Container>> buttons
-    _.forEach(istar.metamodel.containers, function(elementType) {
-        //if specific ui elements are not defined, use default ones
-        var label = elementType.buttonLabel || elementType.name;
-        var tooltip = elementType.buttonTooltip || ('Add ' + elementType.name);
-        var statusText = elementType.buttonStatusText || ('Adding <b>' + elementType.name + '</b>: click on an empty space in the diagram to add a new ' + elementType.name);
-        var image = elementType.name;
+    if (istar.metamodel.containers) {
+        _.forEach(istar.metamodel.containers, function (elementType) {
+            //if specific ui elements are not defined, use default ones
+            var label = elementType.buttonLabel || elementType.name;
+            var tooltip = elementType.buttonTooltip || ('Add ' + elementType.name);
+            var statusText = elementType.buttonStatusText || ('Adding <b>' + elementType.name + '</b>: click on an empty space in the diagram to add a new ' + elementType.name);
+            var image = elementType.name;
 
-        new ui.components.AddButtonDropdownItemView({
-            attributes: {parent: '#add-actor-dropdown'},
-            model: new ui.components.AddButtonModel({
-                action: ui.states.editor.ADDING.ADD_CONTAINER,
-                buttonImage: image,
-                defaultButtonImage: 'DefaultContainer.svg',
-                label: label,
-                name: elementType.name,
-                statusText: statusText,
-                tooltip: tooltip
-            })
-        }).render();
-    });
+            new ui.components.AddButtonDropdownItemView({
+                attributes: {parent: '#add-actor-dropdown'},
+                model: new ui.components.AddButtonModel({
+                    action: ui.states.editor.ADDING.ADD_CONTAINER,
+                    buttonImage: image,
+                    defaultButtonImage: 'DefaultContainer.svg',
+                    label: label,
+                    name: elementType.name,
+                    statusText: statusText,
+                    tooltip: tooltip
+                })
+            }).render();
+        });
+    }
+    else {
+        $('#menu-dropdown-actors').hide();
+    }
 
     //create Add <<ContainerLink>> buttons
-    _.forEach(istar.metamodel.containerLinks, function(linkType) {
-        //if specific ui elements are not defined, use default ones
-        var label = linkType.buttonLabel || (linkType.name);
-        var tooltip = linkType.buttonTooltip || ('Add a ' + linkType.name);
-        var statusText = linkType.buttonStatusText || ('Adding a <b>' + linkType.name + '</b>');
-        var image = linkType.name;
+    if (istar.metamodel.containerLinks) {
+        _.forEach(istar.metamodel.containerLinks, function(linkType) {
+            //if specific ui elements are not defined, use default ones
+            var label = linkType.buttonLabel || (linkType.name);
+            var tooltip = linkType.buttonTooltip || ('Add a ' + linkType.name);
+            var statusText = linkType.buttonStatusText || ('Adding a <b>' + linkType.name + '</b>');
+            var image = linkType.name;
 
-        new ui.components.AddButtonDropdownItemView({
-            attributes: {parent: '#add-actor-link-dropdown'},
-            model: new ui.components.AddButtonModel({
-                action: ui.states.editor.ADDING.ADD_LINK,
-                buttonImage: image,
-                defaultButtonImage: 'DefaultContainerLink.svg',
-                label: label,
-                name: linkType.name,
-                statusText: statusText,
-                tooltip: tooltip
-            })
-        }).render();
-    });
+            new ui.components.AddButtonDropdownItemView({
+                attributes: {parent: '#add-actor-link-dropdown'},
+                model: new ui.components.AddButtonModel({
+                    action: ui.states.editor.ADDING.ADD_LINK,
+                    buttonImage: image,
+                    defaultButtonImage: 'DefaultContainerLink.svg',
+                    label: label,
+                    name: linkType.name,
+                    statusText: statusText,
+                    tooltip: tooltip
+                })
+            }).render();
+        });
+    }
+    else {
+        $('#menu-dropdown-actor-links').hide();
+    }
 
     //create Add <<DependencyLink>> buttons
+    var hasDependency = false;
     _.forEach(istar.metamodel.nodes, function(elementType) {
         if (elementType.canBeDependum ) {
+            hasDependency = true;
 
             //if specific ui elements are not defined, use default ones
             var label = elementType.buttonLabel || (elementType.name + ' dependency');
@@ -81,6 +93,10 @@ ui.components.createAddButtons = function() {
             }).render();
         }
     });
+    if (! hasDependency) {
+        $('#menu-dropdown-dependency-links').hide();
+    }
+
 
     //create Add <<Element>> buttons
     _.forEach(istar.metamodel.nodes, function(elementType) {
@@ -106,47 +122,68 @@ ui.components.createAddButtons = function() {
 
     //create Add <<NodeLink>> buttons
     _.forEach(istar.metamodel.nodeLinks, function(linkType) {
-        if (linkType.changeableLabel) {
-            //create a dropdown button and then create a dropdown item for each possible value of the label
+        if (linkType.canBeManuallyAdded !== false) {
+            if (linkType.changeableLabel) {
+                //create a dropdown button and then create a dropdown item for each possible value of the label
 
-            //if specific ui elements are not defined, use default ones
-            var label = (linkType.buttonLabel && linkType.buttonLabel[0]) || linkType.name;
-            var tooltip = (linkType.buttonTooltip && linkType.buttonTooltip[0]) || ('Add ' + linkType.name);
-            var image = linkType.name;
-
-            new ui.components.AddButtonDropdownView({
-                model: new ui.components.AddButtonModel({
-                    buttonImage: image,
-                    defaultButtonImage: 'DefaultContainerLink.svg',
-                    label: label,
-                    name: linkType.name,
-                    tooltip: tooltip
-                })
-            }).render();
-
-            //create the dropdown items
-            _.forEach(linkType.possibleLabels, function(linkValue, i) {
                 //if specific ui elements are not defined, use default ones
-                var index = i + 1;
-                var label = linkValue
-                if (linkType.buttonLabel && linkType.buttonLabel[index]) {
-                    label = linkType.buttonLabel[index];
-                }
-                var tooltip = ('Add a ' + linkValue + ' ' + linkType.name);
-                if (linkType.buttonTooltip && linkType.buttonTooltip[index]) {
-                    tooltip = linkType.buttonTooltip[index];
-                }
-                var statusText = 'Adding a <b>' + linkValue + ' ' + linkType.name + '</b>';
-                if (linkType.buttonStatusText && linkType.buttonStatusText[index]) {
-                    statusText = linkType.buttonStatusText[index];
-                }
-                var image = linkType.name + '-' + linkValue;
-                if (linkType.buttonImage && linkType.buttonImage[index]) {
-                    image = linkType.buttonImage[index];
-                }
+                var label = (linkType.buttonLabel && linkType.buttonLabel[0]) || linkType.name;
+                var tooltip = (linkType.buttonTooltip && linkType.buttonTooltip[0]) || ('Add ' + linkType.name);
+                var image = linkType.name;
 
-                new ui.components.AddButtonDropdownItemView({
-                    attributes: {parent: '#add-'+ linkType.name +'-dropdown'},
+                new ui.components.AddButtonDropdownView({
+                    model: new ui.components.AddButtonModel({
+                        buttonImage: image,
+                        defaultButtonImage: 'DefaultContainerLink.svg',
+                        label: label,
+                        name: linkType.name,
+                        tooltip: tooltip
+                    })
+                }).render();
+
+                //create the dropdown items
+                _.forEach(linkType.possibleLabels, function (linkValue, i) {
+                    //if specific ui elements are not defined, use default ones
+                    var index = i + 1;
+                    var label = linkValue
+                    if (linkType.buttonLabel && linkType.buttonLabel[index]) {
+                        label = linkType.buttonLabel[index];
+                    }
+                    var tooltip = ('Add a ' + linkValue + ' ' + linkType.name);
+                    if (linkType.buttonTooltip && linkType.buttonTooltip[index]) {
+                        tooltip = linkType.buttonTooltip[index];
+                    }
+                    var statusText = 'Adding a <b>' + linkValue + ' ' + linkType.name + '</b>';
+                    if (linkType.buttonStatusText && linkType.buttonStatusText[index]) {
+                        statusText = linkType.buttonStatusText[index];
+                    }
+                    var image = linkType.name + '-' + linkValue;
+                    if (linkType.buttonImage && linkType.buttonImage[index]) {
+                        image = linkType.buttonImage[index];
+                    }
+
+                    new ui.components.AddButtonDropdownItemView({
+                        attributes: {parent: '#add-' + linkType.name + '-dropdown'},
+                        model: new ui.components.AddButtonModel({
+                            action: ui.states.editor.ADDING.ADD_LINK,
+                            buttonImage: image,
+                            defaultButtonImage: 'DefaultContainerLink.svg',
+                            label: label,
+                            name: linkType.name,
+                            statusText: statusText,
+                            tooltip: tooltip,
+                            value: linkValue
+                        })
+                    }).render();
+                });
+            } else {
+                //if specific ui elements are not defined, use default ones
+                var label = linkType.buttonLabel || linkType.name;
+                var tooltip = linkType.buttonTooltip || ('Add ' + linkType.name);
+                var statusText = linkType.buttonStatusText || ('Adding <b>' + linkType.name + '</b>');
+                var image = linkType.name;
+
+                new ui.components.AddButtonView({
                     model: new ui.components.AddButtonModel({
                         action: ui.states.editor.ADDING.ADD_LINK,
                         buttonImage: image,
@@ -154,30 +191,10 @@ ui.components.createAddButtons = function() {
                         label: label,
                         name: linkType.name,
                         statusText: statusText,
-                        tooltip: tooltip,
-                        value: linkValue
+                        tooltip: tooltip
                     })
                 }).render();
-            });
-        }
-        else {
-            //if specific ui elements are not defined, use default ones
-            var label = linkType.buttonLabel || linkType.name;
-            var tooltip = linkType.buttonTooltip || ('Add ' + linkType.name);
-            var statusText = linkType.buttonStatusText || ('Adding <b>' + linkType.name + '</b>');
-            var image = linkType.name;
-
-            new ui.components.AddButtonView({
-                model: new ui.components.AddButtonModel({
-                    action: ui.states.editor.ADDING.ADD_LINK,
-                    buttonImage: image,
-                    defaultButtonImage: 'DefaultContainerLink.svg',
-                    label: label,
-                    name: linkType.name,
-                    statusText: statusText,
-                    tooltip: tooltip
-                })
-            }).render();
+            }
         }
     });
 
